3a7ed4663822ee87a923ef0efa968c5248027b8d
John Petersen <petersen.john@gmail.com>
2021-06-05 20:55:18 -0700

    finished terraform work

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -14,6 +14,12 @@ provider "aws" {
   region  = var.aws_region
 }
 
+data "archive_file" "source_code" {
+  type        = "zip"
+  source_file = "${path.module}/greet_lambda.py"
+  output_path = "${path.module}/greet_lambda.zip"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -35,11 +41,14 @@ EOF
 }
 
 # Deploy an AWS Lambda Function
+variable "lambda_function_name" {
+  default = "project_two_lambda_function"
+}
 resource "aws_lambda_function" "project_two_lambda" {
   filename      = "greet_lambda.zip"
-  function_name = "project_two_lambda_function"
+  function_name = var.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_handler"
+  handler       = "greet_lambda.lambda_handler"
 
   runtime = "python3.8"
 
@@ -48,4 +57,46 @@ resource "aws_lambda_function" "project_two_lambda" {
       greeting = "Hello Terraform from Terraform!!!"
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
+
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 5
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
